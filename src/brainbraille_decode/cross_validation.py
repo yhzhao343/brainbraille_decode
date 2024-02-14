29135c30daa4ac6e0d521344f27277615f1fab7f
Yuhui <yhzhao343@gmail.com>
2024-02-14 00:21:46 -0500

    change the order of return data tuple to (train, test)

--- src/brainbraille_decode/cross_validation.py
+++ src/brainbraille_decode/cross_validation.py
@@ -1,4 +1,4 @@
-import itertools
+from itertools import combinations
 import math
 import random
 import numpy as np
@@ -22,7 +22,7 @@ class BrainBrailleCVGen():
             raise ValueError(f'sub ({sub}) does not exist in subs ({self.subs}) used when instantiating')
         if n >= selected_index_len:
             raise ValueError(f'n({n}) needs to be smaller than the total number of runs ({selected_index_len}) in sub {sub}')
-        test_index = itertools.combinations(selected_index, n)
+        test_index = combinations(selected_index, n)
         test_index_len = math.comb(selected_index_len, n)
         return selected_index, test_index, test_index_len
 
@@ -41,29 +41,29 @@ class BrainBrailleCVGen():
             for i in range(max_fold_num):
                 test_index_list.append(np.array(test_index[i]))
                 train_index_list.append(np.array(tuple(selected_index_set - set(test_index[i]))))
-        return test_index_list, train_index_list
+        return train_index_list, test_index_list
 
     def sub_adaptive_leave_one_sub_out_test_sub(self, sub=1, n_adapt=1, max_fold_num=200):
-        test_list, train_list, adapt_list = self.sub_independent_leave_one_sub_out_with_calib_test_sub(sub, n_adapt, max_fold_num)
+        train_list, test_list, adapt_list = self.sub_independent_leave_one_sub_out_with_calib_test_sub(sub, n_adapt, max_fold_num)
         for i, (train_list_i, adapt_list_i) in enumerate(zip(train_list, adapt_list)):
             train_list[i] = np.concatenate((train_list_i, adapt_list_i))
-        return test_list, train_list
+        return train_list, test_list
 
     def sub_adaptive_leave_one_sub_out(self, n_adapt=1, max_fold_num=200):
         unique_subs = np.unique(self.subs)
         test_index_list = []
         train_index_list = []
         for sub_i in unique_subs:
-            test_i, train_i = self.sub_adaptive_leave_one_sub_out_test_sub(sub_i, n_adapt, max_fold_num)
+            train_i, test_i = self.sub_adaptive_leave_one_sub_out_test_sub(sub_i, n_adapt, max_fold_num)
             test_index_list.extend(test_i)
             train_index_list.extend(train_i)
-        return test_index_list, train_index_list
+        return train_index_list, test_index_list
 
     def sub_independent_leave_one_sub_out_with_calib_test_sub(self, sub=1, n_calib=1, max_fold_num=200):
-        calib_list, test_list = self.sub_dependent_leave_n_run_out(sub, n_calib, max_fold_num)
-        _, train_list_i = self.sub_independent_leave_one_sub_out_test_sub(sub=1)
+        test_list, calib_list = self.sub_dependent_leave_n_run_out(sub, n_calib, max_fold_num)
+        train_list_i, _ = self.sub_independent_leave_one_sub_out_test_sub(sub=1)
         train_list = train_list_i * len(test_list)
-        return test_list, train_list, calib_list
+        return train_list, test_list, calib_list
 
     def sub_independent_leave_one_sub_out_with_calib(self, sub=1, n_calib=1, max_fold_num_each=200):
         unique_subs = np.unique(self.subs)
@@ -71,21 +71,21 @@ class BrainBrailleCVGen():
         train_index_list = []
         calib_index_list = []
         for sub_i in unique_subs:
-            test_list_i,  train_list_i, calib_list_i = self.sub_independent_leave_one_sub_out_with_calib_test_sub(sub_i)
+            train_list_i, test_list_i, calib_list_i = self.sub_independent_leave_one_sub_out_with_calib_test_sub(sub_i)
             test_index_list.extend(test_list_i)
             train_index_list.extend(train_list_i)
             calib_index_list.extend(calib_list_i)
-        return test_index_list, train_index_list, calib_index_list
+        return train_index_list, test_index_list, calib_index_list
 
     def sub_independent_leave_one_sub_out_test_sub(self, sub=1):
-        return [self.index[self.subs == sub]], [self.index[self.subs != sub]]
+        return [self.index[self.subs != sub]], [self.index[self.subs == sub]]
 
     def sub_independent_leave_one_sub_out(self):
         unique_subs = np.unique(self.subs)
         test_index_list = []
         train_index_list = []
         for sub_i in unique_subs:
-            test_i, train_i = self.sub_independent_leave_one_sub_out_test_sub(sub_i)
+            train_i, test_i = self.sub_independent_leave_one_sub_out_test_sub(sub_i)
             test_index_list.extend(test_i)
             train_index_list.extend(train_i)
-        return test_index_list, train_index_list
+        return train_index_list, test_index_list
